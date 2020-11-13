7b1ddde4b4ea708631acb6f13230ae4aa7a37e81
AdrianaEG <adriana.ngc438@gmail.com>
2020-11-13 20:27:26 -0300

    Traigo los datos de los proyectos desde el front

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import './Projects.css'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+import './Projects.css';
+
 function Projects (){
+    const [projects, setProjects] = useState({});
+    useEffect(()=>{
+        axios.get('https://backendportfolio-adri.herokuapp.com/projects')
+        .then(
+            res => setProjects(res.data)
+        )
+    }, [setProjects])
     return(
         <div className="card" id="projects">
             <div className="content-title">
@@ -9,36 +19,22 @@ function Projects (){
             </div>
             
             <div className="content-projects">
-                <div className="card-project">
-                    <img src="https://i.ibb.co/d6Ds62K/simon-dice.png" alt="imagen de proyecto"></img>
-                    <h3>Simón dice - Juego de memoria</h3>
-                    <p>Juego de memoria desarrollado en Javascript, Html5 y Sass(Scss)</p>
-                    <div className="link-project">
-                        <a href="https://adrianaeg.github.io/simon-says/" target="_blank"> Ver demo </a>
-                        <img src="https://www.flaticon.com/svg/static/icons/svg/2909/2909653.svg" className="icon" alt="projects"></img>
+                {projects.length && projects.map((project, index)=>
+                    <div className="card-project">
+                        <img src={project.image} alt="imagen del proyecto"/>
+                        <h3>{project.name}</h3>
+                        <p>{project.descripcion}</p>
+                        <div className="link-project">
+                            <a href={project.demo} target="_blank"> Ver demo </a>
+                            <img src="https://www.flaticon.com/svg/static/icons/svg/2909/2909653.svg" className="icon" alt="projects"></img>
+                        </div>
+                        <div className="link-project">
+                            <a href={project.code} target="_blank"> Ver código en Github </a>
+                            <img className="icon" src="https://img.icons8.com/dusk/64/000000/github.png"/>
+                        </div>
                     </div>
-                    <div className="link-project">
-                        <a href="https://github.com/AdrianaEG/memotest-v1" target="_blank"> Ver código en Github</a>
-                        <img className="icon" src="https://img.icons8.com/dusk/64/000000/github.png"/>
-                    </div>
-                    
-                </div>
-
-                <div className="card-project">
-                    <img src="https://i.ibb.co/ypkx0jV/clon-spotify.png" alt="imagen de proyecto"></img>
-                    <h3>Clon Spotify</h3>
-                    <p>Maquetado en Css</p>
-                    <div className="link-project">
-                        <a href="https://adrianaeg.github.io/spotify/" target="_blank"> Ver demo </a>
-                        <img src="https://www.flaticon.com/svg/static/icons/svg/2909/2909653.svg" className="icon" alt="projects"></img>
-                    </div>
-                    <div className="link-project">
-                        <a href="https://github.com/AdrianaEG/spotify" target="_blank"> Ver código en Github</a>
-                        <img className="icon" src="https://img.icons8.com/dusk/64/000000/github.png"/>
-                    </div>
-                    
-                </div>
-                
+                )}
+      
             </div>
             
         </div>
