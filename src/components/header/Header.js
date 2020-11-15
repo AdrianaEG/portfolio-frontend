b4b276fa3d97add2da5404dc1291e2f6916bce76
AdrianaEG <adriana.ngc438@gmail.com>
2020-11-15 19:42:41 -0300

    Aumento margen entre cards, agrego icono

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import './header.css';
 function Header(){
     return(
         <header className="menu-left">
-            <div><a href="#index"><img src="https://www.flaticon.com/svg/static/icons/svg/3720/3720773.svg" className="icon" alt="about-me"></img></a></div>
+            <div><a href="#index"><img src="https://www.flaticon.com/svg/static/icons/svg/1508/1508843.svg" className="icon" alt="about-me"></img></a></div>
             <div><a href="#projects"><img src="https://www.flaticon.com/svg/static/icons/svg/2909/2909653.svg" className="icon" alt="projects"></img></a></div>
             <div><a href="#education"><img src="https://www.flaticon.com/svg/static/icons/svg/2987/2987867.svg" className="icon" alt="education"></img></a></div>
             <div><a href="#contact"><img src="https://www.flaticon.com/svg/static/icons/svg/3062/3062634.svg" className="icon" alt="contact"></img></a></div>
